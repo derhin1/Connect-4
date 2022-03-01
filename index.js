fde481a2a257402b003724620aa3afd89b61f177
Derrick Hinh <derhin1@gmail.com>
2022-03-01 16:54:12 -0600

    Added color checking functionality and the token now is placed above any existing token

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ const aboveBoard = document.querySelector('.aboveBoard')
 function createButton(){
     for(let i = 0; i < 7; i++){
     const button = document.createElement('button')
+    button.classList.add(i)
     aboveBoard.appendChild(button)
     }
 }
@@ -70,23 +71,95 @@ function createButton(){
 createButton()
 
 
-// Click button change space color --- currently hard coded to be the bottom left most space
-function colorSpace(event){
-    let selectedCol = document.getElementsByTagName('col')[35]
-    if(event.target.tagName === 'BUTTON')
-    selectedCol.classList.toggle(playerColor)
+// Need function to grab the correct index number for the specific col 
+function startingIndex(event){
+    let indexNum = 0
+    if(event.target.className === '0'){
+        indexNum = 35
+        return indexNum
+    }
+    if(event.target.className === '1'){
+        indexNum = 36
+        return indexNum
+    }
+    if(event.target.className === '2'){
+        indexNum = 37
+        return indexNum
+    }
+    if(event.target.className === '3'){
+        indexNum = 38
+        return indexNum
+    }
+    if(event.target.className === '4'){
+        indexNum = 39
+        return indexNum
+    }
+    if(event.target.className === '5'){
+        indexNum = 40
+        return indexNum
+    }
+    if(event.target.className === '6'){
+        indexNum = 41
+        return indexNum
+    }
+    
 }
 
-// Need function to grab the correct index number for the specific col 
-
-aboveBoard.addEventListener('click', colorSpace)
 
 
 
+// Function to check if index has a colored space already
+// function spaceChecker(){
+//     let selectedCols = document.getElementsByTagName('col')[startingIndex()]
+//     if(selectedCols.className === 'red'){
+//         return true
+//     }
+//     return false
+// }
+
+//Function that to move the index by 7 if the current col is colored
+function indexMover(event){
+    let i = 0;
+    let selectedCols = document.getElementsByTagName('col')[startingIndex(event)]
+    let newIndex = startingIndex(event)
+    while(i < 5){
+        if(selectedCols.className === 'red'){
+             newIndex -= 7;
+             selectedCols = document.getElementsByTagName('col')[newIndex]
+            i++
+        }
+         else{
+             break;
+         }  
+    }
+        
+    
+    return newIndex
+
+}
+
+
+
+// Click button change space color --- currently hard coded to be the bottom left most space
+function colorSpace(event){
+    let selectedCol = document.getElementsByTagName('col')[indexMover(event)]
+    if(event.target.tagName === 'BUTTON'){
+        selectedCol.classList.toggle(playerColor)
+    }
+}
+
+
+aboveBoard.addEventListener('click', function(event){  
+    // startingIndex(event);
+    // spaceChecker(event);
+    colorSpace(event);
+})
 
 
 
 
+
+   
 
 
 
